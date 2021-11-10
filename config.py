986a926ca85e4c5842a464150d745f0358ac6d33
leda84 <laura.dejesus929@gmail.com>
2021-11-10 01:29:32 -0600

    day 2 changes

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -6,12 +8,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Allow outside files/folders to be added to the project
 # base directory
 
+load_dotenv(os.path.join(basedir, '.env'))
+
 class Config():
     """
         Set Config variables for the flash app.
         Using Environment variables where available
         Create config variables if not done already
     """
+
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #to turn off messages for updates in sqlalchemy
