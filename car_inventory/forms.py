2f1326546f1b6ba93076130bbc2cc09a27d6e7de
leda84 <laura.dejesus929@gmail.com>
2021-11-10 09:27:43 -0600

    fixed forms py and html

--- car_inventory/forms.py
+++ car_inventory/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired,Email
 class UserLoginForm(FlaskForm):
     # email,password, submit_button
     email = StringField('Email', validators = [DataRequired(), Email()])
-    password = PasswordField('Password', validators = [DataRequired])
+    password = PasswordField('Password', validators = [DataRequired()])
     submit_button = SubmitField()
